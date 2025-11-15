4516b790ca10efceb3a4e5e4d42ea430e8425623
Adriano Ohana <adriano.ohana@gmnail.com>
2025-11-15 12:52:38 -0300

    Criando o merge entre 2 linkedlists

--- java/src/linkedlist/MergeLinkedList.java
+++ java/src/linkedlist/MergeLinkedList.java
@@ -4,24 +4,42 @@ import linkedlist.base.ListNode;
 
 public class MergeLinkedList {
 
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode newLl = null;
+    /**
+     * Aqui nessa estrutura eu sofri para entender que o curNode estava se movendo "por dentro" da estrutura de objetos
+     * do result. Sem isso eu não criaria um rastro, estava apenas tentando criar uma sequência de novos
+     * apontamentos dos Nodes, mas estava praticamente interando N vezes em l1 e l2, ou seja, tornando o algoritmo
+     * com uma complexidade O(n2)
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode();
+        ListNode curNode = result;
 
-        // Implement rule here
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                curNode.next = l1;
+                l1 = l1.next;
+            } else {
+                curNode.next = l2;
+                l2 = l2.next;
+            }
+            curNode = curNode.next;
+        }
 
-        return newLl;
+        curNode.next = (l1 != null) ? l1 : l2;
+
+        return result.next;
     }
 
     static void main() {
-        var n4 = new ListNode(4, null);
-        var n2 = new ListNode(2, n4);
+        var n3 = new ListNode(5, null);
+        var n2 = new ListNode(5, n3);
         var n1 = new ListNode(1, n2);
 
-        var n24 = new ListNode(4, null);
-        var n23 = new ListNode(3, n24);
-        var n21 = new ListNode(1, n23);
+        var n23 = new ListNode(9, null);
+        var n22 = new ListNode(4, n23);
+        var n21 = new ListNode(1, n22);
 
-        ListNode middle = (new MiddleLinkedList()).middleNode(n1);
+        ListNode middle = (new MergeLinkedList()).mergeTwoLists(n1, n21);
         while (middle != null) {
             System.out.println(middle.val + " ");
             middle = middle.next;
